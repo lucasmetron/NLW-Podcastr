21459b217994b32ad8cf339b176736d2405370a6
lucasmetron <lucasmetron@gmail.com>
2021-04-28 09:05:10 -0300

    subindo arquivos para o git

---
+++ README.md
@@ -0,0 +1,26 @@
+<div align="center">
+  <img src="./public/logo.svg" alt="Podcastr logo">
+</div>
+
+![Apresentation](https://user-images.githubusercontent.com/77129569/115946862-54d38800-a49a-11eb-94f0-aab75e02b316.gif)
+
+## 💻 Project presentation
+
+Podcastr is a podcast platform, built during the next level week event - ReactJS trail
+
+## 🚀 Technologies
+
+Technologies used in the project
+
+- [ReactJS](https://reactjs.org/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Next.js](https://nextjs.org/)
+- [Sass](https://sass-lang.com/)
+- [API](https://github.com/andrealvesz/PodcastrNext)
+
+## 🔖 Layout
+  You can view the project layout through the links below:
+
+  - [Layout Web](https://www.figma.com/file/UwFEntsHpHYJlHNQAQr4gA/Podcastr?node-id=160%3A2761)
+  - Remembering that you need to have a [Figma](https://figma.com) account to access it.
+
---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,123 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+}
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  isLooping: boolean;
+  isShuffling: boolean;
+  play: (episode: Episode) => void;
+  playList: (list: Episode[], index: number) => void;
+  setPlayingState: (state: boolean) => void;
+  togglePlay: () => void;
+  toggleLoop: () => void;
+  toggleShuffle: () => void;
+  playNext: () => void;
+  playPrevius: () => void;
+  clearPlayerState: () => void;
+  hasNext: boolean;
+  hasPrevius: boolean;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+type PlayerContextProviderProps = {
+  children: ReactNode;
+}
+
+export function PlayerContextProvider({children}: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list);
+    setCurrentEpisodeIndex(index);
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function toggleLoop() {
+    setIsLooping(!isLooping);
+  }
+  
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
+  const hasPrevius = currentEpisodeIndex > 0;
+  const hasNext = isShuffling || currentEpisodeIndex + 1 < episodeList.length;
+
+  function playNext() {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }
+  }
+
+  function playPrevius() {
+    if (hasPrevius) {
+      setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+    }
+  }
+
+  return (
+    <PlayerContext.Provider 
+      value={{ 
+        episodeList, 
+        currentEpisodeIndex, 
+        play,
+        playList,
+        playNext, 
+        playPrevius,
+        isPlaying, 
+        isLooping,
+        isShuffling,
+        togglePlay, 
+        toggleLoop,
+        toggleShuffle,
+        setPlayingState,
+        hasNext,
+        hasPrevius,
+        clearPlayerState
+        }}
+      >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
+
+export const usePLayer = () => {
+  return useContext(PlayerContext)
+}
